94904078d01dc56cd4a04896323992e493d7c1fc
Mathias Søby Jensen <darkdawnmsj@gmail.com>
2021-02-05 15:01:50 +0100

    Changed run.sh to work for Mac OS, and added +x permissions to integrationTest.

--- bin/run.sh
+++ bin/run.sh
@@ -2,4 +2,9 @@
 JAR_PATH=./target/eevhe-0.0.1-SNAPSHOT.jar
 DEPENDENCIES="./target/dependency-jars/*"
 
-java -cp "${JAR_PATH};${DEPENDENCIES}" dk.mmj.eevhe.Main "$@"
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    java -cp "${JAR_PATH}:${DEPENDENCIES}" dk.mmj.eevhe.Main "$@"
+
+else
+    java -cp "${JAR_PATH};${DEPENDENCIES}" dk.mmj.eevhe.Main "$@"
+fi
