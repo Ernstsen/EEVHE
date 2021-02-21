fb9df802276f787aa1124b309618d7fe7e37a77e
Johannes Ernstsen <ernstsen.johannes@gmail.com>
2021-02-21 21:03:15 +0100

    fixed DA bin file

--- bin/decryptionAuthority.sh
+++ bin/decryptionAuthority.sh
@@ -3,7 +3,7 @@
 echo "Starting Decryption Authority"
 if [[ $1 =~ ^-?[0-9]+$ ]]
 then
-    ./bin/run.sh --authority --conf=initFiles/"$1" --port=808"$1"
+    ./bin/run.sh --authority --conf=conf/common_input.json --port=808"$1" --id="$1"
 else
     echo "Please supply an integer id for this instance"
 fi
