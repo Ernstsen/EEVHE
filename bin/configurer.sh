c6d529ce2e33af5e77a654b1bcf0d38325c57803
Johannes Ernstsen <ernstsen.johannes@gmail.com>
2021-03-17 10:48:12 +0100

    fixed forgotten naming and removed duplicat runConf

--- bin/configurer.sh
+++ bin/configurer.sh
@@ -2,7 +2,7 @@
 
 if [[ $1 =~ ^-?[0-9]+$ ]]
 then
-    echo "Executing Trusted Dealer"
+    echo "Executing System Configurer"
     ./bin/run.sh --configuration --addresses -1_localhost:8081 -2_localhost:8082 -3_localhost:8083 --outputFolder=conf --time -min=$1
 else
     echo "Please supply an integer representing number of minutes for vote"
