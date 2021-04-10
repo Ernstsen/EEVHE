af5dc1be13d3ef0d112be8453181fbe685080501
Johannes Ernstsen <ernstsen.johannes@gmail.com>
2021-04-10 15:47:03 +0200

    added NotifyItem test to testsuite

--- src/test/java/dk/mmj/eevhe/protocols/ProtocolTestSuite.java
+++ src/test/java/dk/mmj/eevhe/protocols/ProtocolTestSuite.java
@@ -3,6 +3,7 @@ package dk.mmj.eevhe.protocols;
 import dk.mmj.eevhe.protocols.connectors.TestBulletinBoardBroadcaster;
 import dk.mmj.eevhe.protocols.connectors.TestRestPeerCommunicator;
 import dk.mmj.eevhe.protocols.connectors.TestServerStateIncomingChannel;
+import dk.mmj.eevhe.protocols.mvba.TestNotifyItem;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
@@ -14,7 +15,8 @@ import org.junit.runners.Suite;
         TestPedersenVSS.class,
         TestBulletinBoardBroadcaster.class,
         TestRestPeerCommunicator.class,
-        TestServerStateIncomingChannel.class
+        TestServerStateIncomingChannel.class,
+        TestNotifyItem.class
 })
 public class ProtocolTestSuite {
 }
