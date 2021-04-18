5af025256c00408ed174a4091b9eb6deade725d5
Johannes Ernstsen <ernstsen.johannes@gmail.com>
2021-04-18 09:51:09 +0200

    wrote test for timeoutMAp

--- src/test/java/dk/mmj/eevhe/protocols/mvba/MVBATestSuite.java
+++ src/test/java/dk/mmj/eevhe/protocols/mvba/MVBATestSuite.java
@@ -9,7 +9,8 @@ import org.junit.runners.Suite;
         TestByzantineAgreementProtocolImpl.class,
         TestCompositeCommunicator.class,
         TestMultiValuedByzantineAgreementProtocolImpl.class,
-        TestNotifyItem.class
+        TestNotifyItem.class,
+        TestTimeoutMap.class
 })
 public class MVBATestSuite {
 }
